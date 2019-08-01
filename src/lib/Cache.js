a24854c4316e6ff014ed86749f2913b134f4e90d
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-31 21:16:52 -0300

    Creating class for handle cache requests

---
+++ src/lib/Cache.js
@@ -0,0 +1,31 @@
+import Redis from 'ioredis';
+
+class Cache {
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      keyPrefix: 'cache:',
+    });
+  }
+
+  set(key, value) {
+    return this.redis.set(key, JSON.stringify(value), 'EX', 60 * 60 * 24);
+  }
+
+  async get(key) {
+    const cached = await this.redis.get(key);
+
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  /**
+   * Remove key from cache
+   * @param {*} key
+   */
+  invalidate(key) {
+    return this.redis.del(key);
+  }
+}
+
+export default new Cache();
